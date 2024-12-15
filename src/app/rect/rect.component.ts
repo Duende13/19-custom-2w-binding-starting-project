8997e141c4556c983f6141ed5b0336eaacb57cef
duende13 <susana.r.cunado@gmail.com>
2024-12-15 14:49:25 +0100

    first commit

---
+++ src/app/rect/rect.component.ts
@@ -0,0 +1,17 @@
+import { Component, input, model, output } from '@angular/core';
+
+@Component({
+  selector: 'app-rect',
+  standalone: true,
+  imports: [],
+  templateUrl: './rect.component.html',
+  styleUrl: './rect.component.css',
+})
+export class RectComponent {
+  // Todo: Implement custom two-way binding
+  size = model.required<{width: string; height: string}>()
+
+  onReset() {
+    this.size.set({width:'200',height:'100'});
+  }
+}
